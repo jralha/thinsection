8424e262ce4afc817d7f1837208c335d9969acad
jralha <42118093+jralha@users.noreply.github.com>
2020-02-07 13:34:04 -0300

    adds command line arguments

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 #%% Imports
 import os
 import sys
-os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
 import tensorflow as tf
 import glob
 import random
@@ -9,11 +8,33 @@ import numpy as np
 import datetime
 from utils import make_gen
 from utils import define_model
+import argparse
+
+#Parsing args
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu_ids', type=str, default='-1')
+parser.add_argument('--continue_training', type=bool, default=False)
+parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints')
+parser.add_argument('--logs_dir', type=str, default='./logs')
+parser.add_argument('--format', type=str, default='jpg')
+parser.add_argument('--img_height', type=int, default=256)
+parser.add_argument('--img_width', type=int, default=256)
+parser.add_argument('--img_chan', type=int, default=3)
+parser.add_argument('--batch_size', type=int, default=10)
+parser.add_argument('--epoch_count', type=int, default=1500)
+parser.add_argument('--init_epoch', type=int, default=0)
+parser.add_argument('--model_file', type=str, default=None)
+parser.add_argument('--k_fold', type=int, default=1)
+
+parser.add_argument('--run_name', type=str, required=True)
+parser.add_argument('--model', type=str, default=None)
+parser.add_argument('--data_dir', type=str, required=True)
+args = parser.parse_args()
 
 #%% Setting path to dataset and dataset properties.
 ##########################################################
-checks = "checkpoints\\"
-data_dir = os.path.join('dataset\\train\\')
+checks = args.checkpoints_dir+"\\"
+data_dir = os.path.join(args.data_dir)
 image_list = list(glob.glob(data_dir+'*/*.jpg'))
 image_count = len(image_list)
 
@@ -22,20 +43,20 @@ class_names = list(np.unique(classes))
 
 #%% Training parameters.
 ########################################
-RUN_NAME = 'GPU-RESNET-' #no spaces or dots
-CONTINUE = False
-BATCH_SIZE = 100
-IMG_WIDTH = 256
-IMG_HEIGHT = 256
-IMG_CHAN = 3
+RUN_NAME = args.run_name
+CONTINUE = args.continue_training
+BATCH_SIZE = args.batch_size
+IMG_WIDTH = args.img_width
+IMG_HEIGHT = args.img_height
+IMG_CHAN = args.img_chan
 STEPS_PER_EPOCH = np.ceil(image_count/BATCH_SIZE)
-epochs = 1500
+epochs = args.epoch_count
 shape = (IMG_HEIGHT,IMG_WIDTH,IMG_CHAN)
-
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_ids
 
 #%%Data generator and model
 #########################################
-kfold=1
+kfold=args.k_fold
 train_data_gen=[]
 val_data_gen=[]
 seeds=[]
@@ -48,8 +69,13 @@ for fold in range(kfold):
 
 if CONTINUE == False:
     FIRST_EPOCH = 1
-    # model = define_model.cnn_shallow(len(class_names),shape)
-    model = define_model.resnet_model(len(class_names),shape)
+    if args.model == 'resnet':
+        model = define_model.resnet_model(len(class_names),shape)
+    elif args.model == 'vgg':
+        model = define_model.vgg_model(len(class_names),shape)
+    else:
+        model = define_model.cnn_shallow(len(class_names),shape)
+    
 elif CONTINUE == True:
     modelfile = 'best-0677.hdf5'
     FIRST_EPOCH = int(modelfile.split('.')[0].split('-')[-1])
@@ -70,7 +96,7 @@ model.compile(
 #%%Callbacks
 #########################################
 for fold in range(kfold):
-    val_acc = str(round(float('{val_accuracy}'),2))
+    val_acc = '{val_accuracy}'
     if kfold > 1:
         filepath_best=checks+RUN_NAME+"{epoch}-"+val_acc+"-fold-"+str(fold+1)+"-seed-"+str(seeds[fold])+".hdf5"
     elif kfold == 1:
